119f58a1a50014cadd2d5dce32cceae10dfb010c
Luiz Sócrate <luiz@socrate.com.br>
2021-09-05 11:26:59 +0200

    First page

--- client/src/Pages/Home_.elm
+++ client/src/Pages/Home_.elm
@@ -1,10 +1,29 @@
 module Pages.Home_ exposing (view)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (class)
+import UI.Logo
 import View exposing (View)
 
 
 view : View msg
 view =
-    { body = [ Html.text "Hello, world!" ]
+    { body = body
     }
+
+
+body : List (Html msg)
+body =
+    [ header [ class "page-header" ]
+        [ div [ class "header-logo" ]
+            [ UI.Logo.logo ]
+        ]
+    , div [ class "page-content" ] []
+    , footer [ class "page-footer" ]
+        [ small [ class "footer-legal" ]
+            [ p [] [ text "This site is not owned, endorsed or supported by Renegade Game Studios" ]
+            , p [] [ text "The information presented above about Vampire the Masquerade Rivals, both literal and graphical, is © Renegade Game Studio. All Rights reserved." ]
+            , p [] [ text "© 2020 Renegade Game Studios, Paradox Interactive®, Vampire The Masquerade®, World of Darkness®, Copyright 2020 Paradox Interactive AB (publ)." ]
+            ]
+        ]
+    ]
