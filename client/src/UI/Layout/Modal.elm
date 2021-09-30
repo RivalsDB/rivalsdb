b6d27e2fb2615d19154d71545dac0b1d7ab994cf
Luiz Sócrate <luiz@socrate.com.br>
2021-10-01 00:19:14 +0200

    login

--- client/src/UI/Layout/Modal.elm
+++ client/src/UI/Layout/Modal.elm
@@ -1,11 +1,10 @@
 module UI.Layout.Modal exposing (Model, Msg(..), init, update, view)
 
 import Effect exposing (Effect)
-import Html exposing (Html, button, div, form, h1, input, label, text)
+import Html exposing (Html, button, div, form, input, label, p, text)
 import Html.Attributes exposing (class, type_)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Shared
-import Svg exposing (switch)
 
 
 type alias Model =
@@ -56,13 +55,21 @@ view msg =
     div [ class "modal" ]
         [ div [ class "modal-bg", onClick (msg CloseModal) ] []
         , div [ class "modal-content" ]
-            [ h1 [] [ text "Sign In" ]
+            [ p [ class "modal-header" ]
+                [ text "Sign In" ]
+            , div [ class "modal-close", onClick (msg CloseModal) ]
+                [ text "[x] Close" ]
             , form [ onSubmit (msg Submit) ]
-                [ label [ onInput (msg << ChangedEmail) ]
-                    [ text "Email: "
-                    , input [ type_ "email" ] []
+                [ div [ class "modal-form" ]
+                    [ label [ onInput (msg << ChangedEmail) ]
+                        [ text "Email: "
+                        , input [ type_ "email" ] []
+                        ]
                     ]
-                , button [ type_ "submit" ] [ text "Sign in" ]
+                , div [ class "modal-submit" ]
+                    [ button [ type_ "submit" ]
+                        [ text "Sign in" ]
+                    ]
                 ]
             ]
         ]
