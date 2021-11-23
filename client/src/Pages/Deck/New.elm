3e342a16558af99df3bb7f750a68b9cf3f6f142b
Luiz Sócrate <luiz@socrate.com.br>
2021-11-23 23:17:57 +0100

    Extract deckbuilder

--- client/src/Pages/Deck/New.elm
+++ client/src/Pages/Deck/New.elm
@@ -3,24 +3,19 @@ module Pages.Deck.New exposing (Model, Msg, page)
 import API.Decklist
 import Auth
 import Browser.Navigation as Navigation exposing (Key)
-import Cards exposing (Card)
-import Clan exposing (Clan)
-import Deck exposing (DeckPreSave, Decklist, Name(..))
-import Dict
+import Cards
+import Deck exposing (DeckPreSave, Name(..))
 import Effect exposing (Effect)
 import Gen.Params.Deck.New exposing (Params)
 import Gen.Route as Route
-import Html exposing (..)
-import Html.Attributes exposing (checked, class, classList, name, type_)
+import Html exposing (Html, div, li, span, text, ul)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
-import Html.Keyed as Keyed
 import Page
 import Request
-import Shared exposing (Collection)
-import UI.Attribute
-import UI.Card
+import Shared
 import UI.Decklist
-import UI.FilterSelection
+import UI.DecklistOptions as DecklistOptions
 import UI.Icon as Icon
 import UI.Layout.Template
 import View exposing (View)
@@ -43,33 +38,17 @@ page shared req =
 
 
 type alias Model =
-    { stackFilters : UI.FilterSelection.Model Cards.CardStack Msg
-    , primaryFilters : UI.FilterSelection.Model Cards.Trait Msg
-    , secondaryFilters : UI.FilterSelection.Model Cards.Trait Msg
-    , attackTypeFilters : UI.FilterSelection.Model Cards.AttackType Msg
-    , clansFilters : UI.FilterSelection.Model Clan Msg
-    , disciplineFilters : UI.FilterSelection.Model Cards.Discipline Msg
-    , textFilter : Maybe String
-    , showAllFilters : Bool
-    , showCollectionImages : Bool
+    { key : Key
     , deck : DeckPreSave
-    , key : Key
+    , builderOptions : DecklistOptions.Model Msg
     }
 
 
 init : Navigation.Key -> ( Model, Effect Msg )
 init key =
-    ( { stackFilters = UI.FilterSelection.stacks
-      , primaryFilters = UI.FilterSelection.primaryTraits
-      , secondaryFilters = UI.FilterSelection.secondaryTraits
-      , attackTypeFilters = UI.FilterSelection.attackTypes
-      , clansFilters = UI.FilterSelection.clans
-      , disciplineFilters = UI.FilterSelection.disciplines
-      , textFilter = Nothing
-      , showAllFilters = False
-      , showCollectionImages = False
+    ( { key = key
       , deck = Deck.init
-      , key = key
+      , builderOptions = DecklistOptions.init
       }
     , Effect.none
     )
@@ -77,17 +56,7 @@ init key =
 
 type Msg
     = FromShared Shared.Msg
-    | FromStacksFilter (UI.FilterSelection.Msg Cards.CardStack)
-    | FromPrimaryFilter (UI.FilterSelection.Msg Cards.Trait)
-    | FromSecondaryFilter (UI.FilterSelection.Msg Cards.Trait)
-    | FromAttackTypesFilter (UI.FilterSelection.Msg Cards.AttackType)
-    | FromClansFilter (UI.FilterSelection.Msg Clan)
-    | FromDisciplinesFilter (UI.FilterSelection.Msg Cards.Discipline)
-    | TextFilterChanged String
-    | ToggleShowAllFilters
-    | ClearFilters
-    | ToggleShowCollectionImages
-    | ChangedDecklist ( Cards.Card, Int )
+    | FromBuilderOptions DecklistOptions.Msg
     | ChoseLeader Cards.Faction
     | Save
     | SavedDecklist API.Decklist.ResultCreate
@@ -99,68 +68,18 @@ type Msg
 update : Auth.User -> Msg -> Model -> ( Model, Effect Msg )
 update user msg model =
     case msg of
-        TextFilterChanged text ->
-            let
-                cleanText =
-                    text |> String.trim |> String.toLower
-            in
-            ( { model
-                | textFilter =
-                    if cleanText == "" then
-                        Nothing
-
-                    else
-                        Just cleanText
-              }
-            , Effect.none
-            )
-
         FromShared subMsg ->
             ( model, Effect.fromShared subMsg )
 
-        FromStacksFilter subMsg ->
-            ( { model | stackFilters = UI.FilterSelection.update subMsg model.stackFilters }, Effect.none )
-
-        FromPrimaryFilter subMsg ->
-            ( { model | primaryFilters = UI.FilterSelection.update subMsg model.primaryFilters }, Effect.none )
-
-        FromSecondaryFilter subMsg ->
-            ( { model | secondaryFilters = UI.FilterSelection.update subMsg model.secondaryFilters }, Effect.none )
-
-        FromAttackTypesFilter subMsg ->
-            ( { model | attackTypeFilters = UI.FilterSelection.update subMsg model.attackTypeFilters }, Effect.none )
-
-        FromClansFilter subMsg ->
-            ( { model | clansFilters = UI.FilterSelection.update subMsg model.clansFilters }, Effect.none )
-
-        FromDisciplinesFilter subMsg ->
-            ( { model | disciplineFilters = UI.FilterSelection.update subMsg model.disciplineFilters }, Effect.none )
-
-        ClearFilters ->
-            ( { model
-                | stackFilters = UI.FilterSelection.stacks
-                , primaryFilters = UI.FilterSelection.primaryTraits
-                , secondaryFilters = UI.FilterSelection.secondaryTraits
-                , attackTypeFilters = UI.FilterSelection.attackTypes
-                , clansFilters = UI.FilterSelection.clans
-                , disciplineFilters = UI.FilterSelection.disciplines
-                , textFilter = Nothing
-              }
-            , Effect.none
-            )
-
-        ToggleShowAllFilters ->
-            ( { model | showAllFilters = not model.showAllFilters }, Effect.none )
-
-        ToggleShowCollectionImages ->
-            ( { model | showCollectionImages = not model.showCollectionImages }, Effect.none )
-
-        ChangedDecklist change ->
+        FromBuilderOptions (DecklistOptions.ChangedDecklist change) ->
             let
                 oldDeck =
                     model.deck
             in
             ( { model | deck = { oldDeck | decklist = Deck.setCard oldDeck.decklist change } }, Effect.none )
+
+        FromBuilderOptions subMsg ->
+            ( { model | builderOptions = DecklistOptions.update subMsg model.builderOptions }, Effect.none )
 
         ChoseLeader leader ->
             let
@@ -237,47 +156,8 @@ view shared model =
             [ viewActions
             , div [ class "deckbldr-decklist" ]
                 [ UI.Decklist.viewCreate decklistActions model.deck ]
-            , div [ class "deckbldr-choices" ]
-                [ div [ class "fltrhead" ]
-                    [ h2 [ class "fltrhead-name" ] [ text "Filters " ]
-                    , span [ onClick ToggleShowAllFilters, class "fltrhead-primary", class "fltrhead-action" ]
-                        [ text <|
-                            if model.showAllFilters then
-                                "(hide filters)"
-
-                            else
-                                "(show more)"
-                        ]
-                    , span [ onClick ClearFilters, class "fltrhead-secondary", class "fltrhead-action" ] [ text "Clear filters" ]
-                    ]
-                , div [ class "deckbldr-filters" ] <|
-                    List.intersperse (text " ")
-                        (viewMainFilters model
-                            ++ (if model.showAllFilters then
-                                    viewSecondaryFilters model
-
-                                else
-                                    []
-                               )
-                        )
-                , div [ class "fltrhead" ]
-                    [ h2 [ class "fltrhead-name" ] [ text "Cards " ]
-                    , span [ onClick ToggleShowCollectionImages, class "fltrhead-primary", class "fltrhead-action" ]
-                        [ text <|
-                            if model.showCollectionImages then
-                                "(hide images)"
-
-                            else
-                                "(show images)"
-                        ]
-                    ]
-                , div [ class "deckbldr-collection" ] <|
-                    if model.showCollectionImages then
-                        viewCardListImages shared.collection model
-
-                    else
-                        viewCardList shared.collection model
-                ]
+            , div [ class "deckbldr-choices" ] <|
+                DecklistOptions.view shared.collection FromBuilderOptions model.builderOptions model.deck.decklist
             ]
         ]
 
@@ -292,160 +172,3 @@ viewActions =
                 ]
             ]
         ]
-
-
-viewMainFilters : Model -> List (Html Msg)
-viewMainFilters model =
-    [ div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view FromStacksFilter model.stackFilters ]
-    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view FromPrimaryFilter model.primaryFilters ]
-    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view FromClansFilter model.clansFilters ]
-    ]
-
-
-viewSecondaryFilters : Model -> List (Html Msg)
-viewSecondaryFilters model =
-    [ div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view FromSecondaryFilter model.secondaryFilters ]
-    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view FromAttackTypesFilter model.attackTypeFilters ]
-    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view FromDisciplinesFilter model.disciplineFilters ]
-    ]
-
-
-viewCardList : Collection -> Model -> List (Html Msg)
-viewCardList collection model =
-    [ Keyed.ul [ class "deckbldr-collectionitems--rows" ] <|
-        List.map (\c -> ( Cards.id c, viewCardListRow model.deck.decklist c )) <|
-            cardsToShow collection model
-    ]
-
-
-viewCardListRow : Decklist -> Card -> Html Msg
-viewCardListRow deck card =
-    li [ class "deckbldr-collectionitem--row" ]
-        [ span [ class "deckbldr-rowpiece_quant--row" ] [ viewQuantityPicker card (Deck.copiesInDeck deck card) ]
-        , span [ class "deckbldr-rowpiece_name" ] [ text <| Cards.name card ]
-        , span [ class "deckbldr-rowpiece_props" ]
-            (case card of
-                Cards.FactionCard props ->
-                    [ span [ class "deckbldr-rowpiece_clan" ] [ Icon.clan Icon.Negative props.clan ]
-                    , span [ class "deckbldr-rowpiece_bp" ] [ UI.Attribute.bloodPotency props.bloodPotency ]
-                    , span [ class "deckbldr-rowpiece_physical" ] [ UI.Attribute.physical props.physical ]
-                    , span [ class "deckbldr-rowpiece_social" ] [ UI.Attribute.social props.social ]
-                    , span [ class "deckbldr-rowpiece_mental" ] [ UI.Attribute.mental props.mental ]
-                    , span [ class "deckbldr-rowpiece_disciplines" ]
-                        (props.disciplines
-                            |> List.map (span [ class "deckbldr-rowpiece_discipline" ] << List.singleton << Icon.discipline)
-                        )
-                    ]
-
-                Cards.LibraryCard props ->
-                    [ span [ class "deckbldr-rowpiece_clan" ]
-                        [ props.clan |> Maybe.map (Icon.clan Icon.Negative) |> Maybe.withDefault (text "")
-                        ]
-                    , span [ class "deckbldr-rowpiece_bp" ] [ UI.Attribute.bloodPotencyRequirement props.bloodPotency ]
-                    , span [ class "deckbldr-rowpiece_damage" ] [ UI.Attribute.damage props.damage ]
-                    , span [ class "deckbldr-rowpiece_shields" ] [ UI.Attribute.shield props.shield ]
-                    , span [ class "deckbldr-rowpiece_types" ]
-                        (props.attackType
-                            |> List.map (span [ class "deckbldr-rowpiece_type" ] << List.singleton << Icon.attackType)
-                        )
-                    ]
-
-                _ ->
-                    []
-            )
-        ]
-
-
-viewQuantityPicker : Card -> Int -> Html Msg
-viewQuantityPicker card copiesInDeck =
-    div [ class "quantpick", class ("quantpick--" ++ (String.fromInt <| Cards.maxPerDeck card)) ] <|
-        (Cards.maxPerDeck card
-            |> List.range 0
-            |> List.map
-                (\n ->
-                    label
-                        [ class "quantpick-option"
-                        , classList [ ( "quantpick-option--active", n == copiesInDeck ) ]
-                        ]
-                        [ text <| String.fromInt n
-                        , input
-                            [ type_ "radio"
-                            , name <| "count-" ++ Cards.id card
-                            , checked <| n == copiesInDeck
-                            , onClick <| ChangedDecklist ( card, n )
-                            ]
-                            []
-                        ]
-                )
-        )
-
-
-viewCardListImages : Collection -> Model -> List (Html Msg)
-viewCardListImages collection model =
-    [ Keyed.ul [ class "deckbldr-collectionitems--images" ] <|
-        List.map (\c -> ( Cards.id c, viewCardListImage model.deck.decklist c )) <|
-            cardsToShow collection model
-    ]
-
-
-viewCardListImage : Decklist -> Card -> Html Msg
-viewCardListImage deck card =
-    li [ class "deckbldr-collectionitem--image" ]
-        [ UI.Card.lazy card
-        , div [ class "deckbldr-rowpiece_quant--image" ] [ viewQuantityPicker card (Deck.copiesInDeck deck card) ]
-        ]
-
-
-cardsToShow : Collection -> Model -> List Card
-cardsToShow collection model =
-    filteredCards collection model |> List.sortWith cardSort
-
-
-filteredCards : Collection -> Model -> List Card
-filteredCards collection model =
-    let
-        cards =
-            Dict.values collection
-    in
-    case model.textFilter of
-        Nothing ->
-            List.filter (filterFlags model) cards
-
-        Just needle ->
-            List.filter (Cards.findTextInCard needle) cards
-                |> List.filter (filterFlags model)
-
-
-filterFlags : Model -> Card -> Bool
-filterFlags model card =
-    UI.FilterSelection.isAllowed Cards.clanRequirement model.clansFilters card
-        && UI.FilterSelection.isAllowed Cards.traits model.secondaryFilters card
-        && UI.FilterSelection.isAllowed Cards.stack model.stackFilters card
-        && UI.FilterSelection.isAllowed Cards.discipline model.disciplineFilters card
-        && UI.FilterSelection.isAllowed Cards.traits model.primaryFilters card
-        && UI.FilterSelection.isAllowed Cards.attackTypes model.attackTypeFilters card
-
-
-cardSort : Card -> Card -> Order
-cardSort a b =
-    let
-        stack card =
-            case card of
-                Cards.AgendaCard _ ->
-                    1
-
-                Cards.HavenCard _ ->
-                    2
-
-                Cards.FactionCard _ ->
-                    3
-
-                Cards.LibraryCard _ ->
-                    4
-    in
-    case compare (stack a) (stack b) of
-        EQ ->
-            compare (Cards.name a) (Cards.name b)
-
-        ord ->
-            ord
---
+++ client/src/UI/DecklistOptions.elm
@@ -0,0 +1,298 @@
+module UI.DecklistOptions exposing (Model, Msg(..), init, update, view)
+
+import Cards exposing (Card)
+import Clan exposing (Clan)
+import Deck exposing (Decklist)
+import Dict
+import Html exposing (Html, div, h2, input, label, li, span, text)
+import Html.Attributes exposing (checked, class, classList, name, type_)
+import Html.Events exposing (onClick)
+import Html.Keyed as Keyed
+import Shared exposing (Collection)
+import UI.Attribute
+import UI.Card
+import UI.FilterSelection
+import UI.Icon as Icon
+
+
+type alias Model msg =
+    { stackFilters : UI.FilterSelection.Model Cards.CardStack msg
+    , primaryFilters : UI.FilterSelection.Model Cards.Trait msg
+    , secondaryFilters : UI.FilterSelection.Model Cards.Trait msg
+    , attackTypeFilters : UI.FilterSelection.Model Cards.AttackType msg
+    , clansFilters : UI.FilterSelection.Model Clan msg
+    , disciplineFilters : UI.FilterSelection.Model Cards.Discipline msg
+    , textFilter : Maybe String
+    , showAllFilters : Bool
+    , showCollectionImages : Bool
+    }
+
+
+init : Model msg
+init =
+    { stackFilters = UI.FilterSelection.stacks
+    , primaryFilters = UI.FilterSelection.primaryTraits
+    , secondaryFilters = UI.FilterSelection.secondaryTraits
+    , attackTypeFilters = UI.FilterSelection.attackTypes
+    , clansFilters = UI.FilterSelection.clans
+    , disciplineFilters = UI.FilterSelection.disciplines
+    , textFilter = Nothing
+    , showAllFilters = False
+    , showCollectionImages = False
+    }
+
+
+type Msg
+    = ChangedDecklist ( Card, Int )
+    | ClearFilters
+    | FromAttackTypesFilter (UI.FilterSelection.Msg Cards.AttackType)
+    | FromClansFilter (UI.FilterSelection.Msg Clan)
+    | FromDisciplinesFilter (UI.FilterSelection.Msg Cards.Discipline)
+    | FromPrimaryFilter (UI.FilterSelection.Msg Cards.Trait)
+    | FromSecondaryFilter (UI.FilterSelection.Msg Cards.Trait)
+    | FromStacksFilter (UI.FilterSelection.Msg Cards.CardStack)
+    | ToggleShowAllFilters
+    | ToggleShowCollectionImages
+
+
+update : Msg -> Model msg -> Model msg
+update msg model =
+    case msg of
+        FromStacksFilter subMsg ->
+            { model | stackFilters = UI.FilterSelection.update subMsg model.stackFilters }
+
+        FromPrimaryFilter subMsg ->
+            { model | primaryFilters = UI.FilterSelection.update subMsg model.primaryFilters }
+
+        FromSecondaryFilter subMsg ->
+            { model | secondaryFilters = UI.FilterSelection.update subMsg model.secondaryFilters }
+
+        FromAttackTypesFilter subMsg ->
+            { model | attackTypeFilters = UI.FilterSelection.update subMsg model.attackTypeFilters }
+
+        FromClansFilter subMsg ->
+            { model | clansFilters = UI.FilterSelection.update subMsg model.clansFilters }
+
+        FromDisciplinesFilter subMsg ->
+            { model | disciplineFilters = UI.FilterSelection.update subMsg model.disciplineFilters }
+
+        ClearFilters ->
+            { model
+                | stackFilters = UI.FilterSelection.stacks
+                , primaryFilters = UI.FilterSelection.primaryTraits
+                , secondaryFilters = UI.FilterSelection.secondaryTraits
+                , attackTypeFilters = UI.FilterSelection.attackTypes
+                , clansFilters = UI.FilterSelection.clans
+                , disciplineFilters = UI.FilterSelection.disciplines
+                , textFilter = Nothing
+            }
+
+        ToggleShowAllFilters ->
+            { model | showAllFilters = not model.showAllFilters }
+
+        ToggleShowCollectionImages ->
+            { model | showCollectionImages = not model.showCollectionImages }
+
+        ChangedDecklist _ ->
+            model
+
+
+view : Collection -> (Msg -> msg) -> Model msg -> Decklist -> List (Html msg)
+view collection msg data decklist =
+    [ div [ class "fltrhead" ]
+        [ h2 [ class "fltrhead-name" ] [ text "Filters " ]
+        , span [ onClick (msg ToggleShowAllFilters), class "fltrhead-primary", class "fltrhead-action" ]
+            [ text <|
+                if data.showAllFilters then
+                    "(hide filters)"
+
+                else
+                    "(show more)"
+            ]
+        , span [ onClick (msg ClearFilters), class "fltrhead-secondary", class "fltrhead-action" ] [ text "Clear filters" ]
+        ]
+    , div [ class "deckbldr-filters" ] <|
+        List.intersperse (text " ")
+            (viewMainFilters msg data
+                ++ (if data.showAllFilters then
+                        viewSecondaryFilters msg data
+
+                    else
+                        []
+                   )
+            )
+    , div [ class "fltrhead" ]
+        [ h2 [ class "fltrhead-name" ] [ text "Cards " ]
+        , span [ onClick (msg ToggleShowCollectionImages), class "fltrhead-primary", class "fltrhead-action" ]
+            [ text <|
+                if data.showCollectionImages then
+                    "(hide images)"
+
+                else
+                    "(show images)"
+            ]
+        ]
+    , div [ class "deckbldr-collection" ] <|
+        if data.showCollectionImages then
+            viewCardListImages collection msg data decklist
+
+        else
+            viewCardList collection msg data decklist
+    ]
+
+
+viewMainFilters : (Msg -> msg) -> Model msg -> List (Html msg)
+viewMainFilters msg data =
+    [ div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view (msg << FromStacksFilter) data.stackFilters ]
+    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view (msg << FromPrimaryFilter) data.primaryFilters ]
+    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view (msg << FromClansFilter) data.clansFilters ]
+    ]
+
+
+viewSecondaryFilters : (Msg -> msg) -> Model msg -> List (Html msg)
+viewSecondaryFilters msg data =
+    [ div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view (msg << FromSecondaryFilter) data.secondaryFilters ]
+    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view (msg << FromAttackTypesFilter) data.attackTypeFilters ]
+    , div [ class "deckbldr-flaggroup" ] [ UI.FilterSelection.view (msg << FromDisciplinesFilter) data.disciplineFilters ]
+    ]
+
+
+viewCardListImages : Collection -> (Msg -> msg) -> Model msg -> Decklist -> List (Html msg)
+viewCardListImages collection msg data decklist =
+    [ Keyed.ul [ class "deckbldr-collectionitems--images" ] <|
+        List.map (\c -> ( Cards.id c, viewCardListImage msg decklist c )) <|
+            cardsToShow collection data
+    ]
+
+
+viewCardListImage : (Msg -> msg) -> Decklist -> Card -> Html msg
+viewCardListImage msg deck card =
+    li [ class "deckbldr-collectionitem--image" ]
+        [ UI.Card.lazy card
+        , div [ class "deckbldr-rowpiece_quant--image" ] [ viewQuantityPicker msg card (Deck.copiesInDeck deck card) ]
+        ]
+
+
+viewCardList : Collection -> (Msg -> msg) -> Model msg -> Decklist -> List (Html msg)
+viewCardList collection msg data decklist =
+    [ Keyed.ul [ class "deckbldr-collectionitems--rows" ] <|
+        List.map (\c -> ( Cards.id c, viewCardListRow msg decklist c )) <|
+            cardsToShow collection data
+    ]
+
+
+viewCardListRow : (Msg -> msg) -> Decklist -> Card -> Html msg
+viewCardListRow msg deck card =
+    li [ class "deckbldr-collectionitem--row" ]
+        [ span [ class "deckbldr-rowpiece_quant--row" ] [ viewQuantityPicker msg card (Deck.copiesInDeck deck card) ]
+        , span [ class "deckbldr-rowpiece_name" ] [ text <| Cards.name card ]
+        , span [ class "deckbldr-rowpiece_props" ]
+            (case card of
+                Cards.FactionCard props ->
+                    [ span [ class "deckbldr-rowpiece_clan" ] [ Icon.clan Icon.Negative props.clan ]
+                    , span [ class "deckbldr-rowpiece_bp" ] [ UI.Attribute.bloodPotency props.bloodPotency ]
+                    , span [ class "deckbldr-rowpiece_physical" ] [ UI.Attribute.physical props.physical ]
+                    , span [ class "deckbldr-rowpiece_social" ] [ UI.Attribute.social props.social ]
+                    , span [ class "deckbldr-rowpiece_mental" ] [ UI.Attribute.mental props.mental ]
+                    , span [ class "deckbldr-rowpiece_disciplines" ]
+                        (props.disciplines
+                            |> List.map (span [ class "deckbldr-rowpiece_discipline" ] << List.singleton << Icon.discipline)
+                        )
+                    ]
+
+                Cards.LibraryCard props ->
+                    [ span [ class "deckbldr-rowpiece_clan" ]
+                        [ props.clan |> Maybe.map (Icon.clan Icon.Negative) |> Maybe.withDefault (text "")
+                        ]
+                    , span [ class "deckbldr-rowpiece_bp" ] [ UI.Attribute.bloodPotencyRequirement props.bloodPotency ]
+                    , span [ class "deckbldr-rowpiece_damage" ] [ UI.Attribute.damage props.damage ]
+                    , span [ class "deckbldr-rowpiece_shields" ] [ UI.Attribute.shield props.shield ]
+                    , span [ class "deckbldr-rowpiece_types" ]
+                        (props.attackType
+                            |> List.map (span [ class "deckbldr-rowpiece_type" ] << List.singleton << Icon.attackType)
+                        )
+                    ]
+
+                _ ->
+                    []
+            )
+        ]
+
+
+viewQuantityPicker : (Msg -> msg) -> Card -> Int -> Html msg
+viewQuantityPicker msg card copiesInDeck =
+    div [ class "quantpick", class ("quantpick--" ++ (String.fromInt <| Cards.maxPerDeck card)) ] <|
+        (Cards.maxPerDeck card
+            |> List.range 0
+            |> List.map
+                (\n ->
+                    label
+                        [ class "quantpick-option"
+                        , classList [ ( "quantpick-option--active", n == copiesInDeck ) ]
+                        ]
+                        [ text <| String.fromInt n
+                        , input
+                            [ type_ "radio"
+                            , name <| "count-" ++ Cards.id card
+                            , checked <| n == copiesInDeck
+                            , onClick <| msg <| ChangedDecklist ( card, n )
+                            ]
+                            []
+                        ]
+                )
+        )
+
+
+cardsToShow : Collection -> Model msg -> List Card
+cardsToShow collection data =
+    filteredCards collection data |> List.sortWith cardSort
+
+
+filteredCards : Collection -> Model msg -> List Card
+filteredCards collection data =
+    let
+        cards =
+            Dict.values collection
+    in
+    case data.textFilter of
+        Nothing ->
+            List.filter (filterFlags data) cards
+
+        Just needle ->
+            List.filter (Cards.findTextInCard needle) cards
+                |> List.filter (filterFlags data)
+
+
+filterFlags : Model msg -> Card -> Bool
+filterFlags data card =
+    UI.FilterSelection.isAllowed Cards.clanRequirement data.clansFilters card
+        && UI.FilterSelection.isAllowed Cards.traits data.secondaryFilters card
+        && UI.FilterSelection.isAllowed Cards.stack data.stackFilters card
+        && UI.FilterSelection.isAllowed Cards.discipline data.disciplineFilters card
+        && UI.FilterSelection.isAllowed Cards.traits data.primaryFilters card
+        && UI.FilterSelection.isAllowed Cards.attackTypes data.attackTypeFilters card
+
+
+cardSort : Card -> Card -> Order
+cardSort a b =
+    let
+        stack card =
+            case card of
+                Cards.AgendaCard _ ->
+                    1
+
+                Cards.HavenCard _ ->
+                    2
+
+                Cards.FactionCard _ ->
+                    3
+
+                Cards.LibraryCard _ ->
+                    4
+    in
+    case compare (stack a) (stack b) of
+        EQ ->
+            compare (Cards.name a) (Cards.name b)
+
+        ord ->
+            ord
