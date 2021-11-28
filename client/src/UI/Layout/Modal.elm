35413e20c95d6256bcb7be97af05cf57bfb7bc50
Luiz Sócrate <luiz@socrate.com.br>
2021-11-28 17:58:08 +0100

    Modal styles

--- client/src/UI/Layout/Modal.elm
+++ client/src/UI/Layout/Modal.elm
@@ -1,7 +1,7 @@
 module UI.Layout.Modal exposing (view)
 
 import Html exposing (Html, button, div, form, input, label, p, text)
-import Html.Attributes exposing (class, type_)
+import Html.Attributes exposing (class, for, id, placeholder, type_)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Shared
 
@@ -9,20 +9,25 @@ import Shared
 view : (Shared.Msg -> msg) -> Html msg
 view msg =
     div [ class "modal" ]
-        [ div [ class "modal-bg", onClick (msg Shared.ModalClose) ] []
-        , div [ class "modal-content" ]
-            [ p [ class "modal-header" ]
+        [ div [ class "modal__bg", onClick (msg Shared.ModalClose) ] []
+        , div [ class "modal__content" ]
+            [ p [ class "modal__header" ]
                 [ text "Sign In" ]
-            , div [ class "modal-close", onClick (msg Shared.ModalClose) ]
+            , div [ class "modal__close", onClick (msg Shared.ModalClose) ]
                 [ text "[x] Close" ]
             , form [ onSubmit (msg Shared.ModalSubmit) ]
-                [ div [ class "modal-form" ]
-                    [ label [ onInput (msg << Shared.ModalChangedEmail) ]
-                        [ text "Email: "
-                        , input [ type_ "email" ] []
+                [ div [ class "modal__form" ]
+                    [ label [ for "email", class "modal__label" ] [ text "Email:" ]
+                    , input
+                        [ type_ "email"
+                        , id "email"
+                        , placeholder "uncle.smelly@schreck.net"
+                        , onInput (msg << Shared.ModalChangedEmail)
+                        , class "modal__input"
                         ]
+                        []
                     ]
-                , div [ class "modal-submit" ]
+                , div [ class "modal__submit" ]
                     [ button [ type_ "submit" ]
                         [ text "Sign in" ]
                     ]
