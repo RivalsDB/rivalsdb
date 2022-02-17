810ae9f2c529ba42c4f409b520422844e6b15528
Luiz Sócrate <luiz@socrate.com.br>
2022-02-17 22:32:54 +0100

    Sort announcements latest first

--- server/src/db/private/announcements.ts
+++ server/src/db/private/announcements.ts
@@ -10,7 +10,8 @@ export async function fetchPublishedAnnoncements(): Promise<Announcement[]> {
   const rows = await db.query(sql`
     SELECT announcement_id, markdown, published_at
     FROM announcements
-    WHERE published_at <= NOW();
+    WHERE published_at <= NOW()
+    ORDER BY published_at DESC;
   `);
   return rows.map(rowToEntity);
 }
