f2f66ab49f9d9382a77756353b081807beede95b
Luiz Sócrate <luiz@socrate.com.br>
2022-02-19 14:38:52 +0100

    Add patreon button to footer

--- client/src/UI/Layout/Footer.elm
+++ client/src/UI/Layout/Footer.elm
@@ -1,13 +1,17 @@
 module UI.Layout.Footer exposing (view)
 
-import Html exposing (Html, footer, p, small, text)
+import Html exposing (Html, div, footer, p, small, text)
 import Html.Attributes exposing (class)
+import UI.PatreonButton
 
 
 view : Html msg
 view =
-    footer [ class "page-footer" ]
-        [ small [ class "footer-legal" ]
+    footer [ class "page-footer", class "footer" ]
+        [ div [ class "footer__patreon" ]
+            [ UI.PatreonButton.patreonButton
+            ]
+        , small [ class "footer__legal" ]
             [ p [] [ text "This site is not owned, endorsed or supported by Renegade Game Studios" ]
             , p [] [ text "The information presented above about Vampire the Masquerade Rivals, both literal and graphical, is © Renegade Game Studio. All Rights reserved." ]
             , p [] [ text "© 2020 Renegade Game Studios, Paradox Interactive®, Vampire The Masquerade®, World of Darkness®, Copyright 2020 Paradox Interactive AB (publ)." ]
---
+++ client/src/UI/PatreonButton.elm
@@ -0,0 +1,35 @@
+module UI.PatreonButton exposing (patreonButton)
+
+import Html exposing (Html, a, div, span, text)
+import Html.Attributes exposing (attribute, class, href, rel, tabindex, target, type_)
+import Svg exposing (circle, g, rect, svg)
+import Svg.Attributes exposing (cx, cy, height, r, viewBox, width, x, y)
+
+
+patreonButton : Html msg
+patreonButton =
+    div [ class "patreon" ]
+        [ a
+            [ class "patreon__button"
+            , tabindex 0
+            , type_ "button"
+            , href "https://www.patreon.com/bePatron?u=2418485"
+            , attribute "role" "button"
+            , target "_blank"
+            , rel "noopener noreferrer"
+            ]
+            [ div [ class "patreon__w1" ]
+                [ div [ class "patreon__w2" ]
+                    [ span [ class "patreon__w3" ]
+                        [ svg [ viewBox "0 0 569 546", class "patreon__svg" ]
+                            [ g []
+                                [ circle [ cx "362.589996", cy "204.589996", r "204.589996" ] []
+                                , rect [ height "545.799988", width "100", x "0", y "0" ] []
+                                ]
+                            ]
+                        ]
+                    ]
+                , div [ class "patreon__txt" ] [ text "Become a patron" ]
+                ]
+            ]
+        ]
