2b2f70f140e33bfe58bee775682d499eee1ed29e
Luiz Sócrate <luiz@socrate.com.br>
2022-02-19 15:02:05 +0100

    Fix patreon button take 2

--- client/src/UI/PatreonButton.elm
+++ client/src/UI/PatreonButton.elm
@@ -1,35 +1,35 @@
 module UI.PatreonButton exposing (patreonButton)
 
 import Html exposing (Html, a, div, span, text)
-import Html.Attributes exposing (attribute, class, href, rel, tabindex, target, type_)
+import Html.Attributes as Attr
 import Svg exposing (circle, g, rect, svg)
-import Svg.Attributes exposing (cx, cy, height, r, viewBox, width, x, y)
+import Svg.Attributes exposing (cx, cy, fill, height, r, strokeWidth, viewBox, width, x, y)
 
 
 patreonButton : Html msg
 patreonButton =
-    div [ class "patreon" ]
+    div [ Attr.class "patreon" ]
         [ a
-            [ class "patreon__button"
-            , tabindex 0
-            , type_ "button"
-            , href "https://www.patreon.com/bePatron?u=2418485"
-            , attribute "role" "button"
-            , target "_blank"
-            , rel "noopener noreferrer"
+            [ Attr.class "patreon__button"
+            , Attr.tabindex 0
+            , Attr.type_ "button"
+            , Attr.href "https://www.patreon.com/bePatron?u=2418485"
+            , Attr.attribute "role" "button"
+            , Attr.target "_blank"
+            , Attr.rel "noopener noreferrer"
             ]
-            [ div [ class "patreon__w1" ]
-                [ div [ class "patreon__w2" ]
-                    [ span [ class "patreon__w3" ]
-                        [ svg [ viewBox "0 0 569 546", Svg.Attributes.class "patreon__svg" ]
-                            [ g []
-                                [ circle [ cx "362.589996", cy "204.589996", r "204.589996" ] []
-                                , rect [ height "545.799988", width "100", x "0", y "0" ] []
+            [ div [ Attr.class "patreon__w1" ]
+                [ div [ Attr.class "patreon__w2" ]
+                    [ span [ Attr.class "patreon__w3" ]
+                        [ svg [ viewBox "0 0 569 546" ]
+                            [ g [ strokeWidth "1.2px" ]
+                                [ circle [ cx "362.589996", cy "204.589996", r "204.589996", fill "#000000" ] []
+                                , rect [ height "545.799988", width "100", x "0", y "0", fill "#000000" ] []
                                 ]
                             ]
                         ]
                     ]
-                , div [ class "patreon__txt" ] [ text "Become a patron" ]
+                , div [ Attr.class "patreon__txt" ] [ text "Become a patron" ]
                 ]
             ]
         ]
