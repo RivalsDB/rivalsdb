567fa82ea45060ae78c3a447ad9c88d90031d3a9
Luiz Sócrate <luiz@socrate.com.br>
2022-06-25 10:44:24 +0200

    Update index.ts
    Remove ambiguous characters from id generator

--- shared/id/index.ts
+++ shared/id/index.ts
@@ -1,7 +1,7 @@
 import { customAlphabet } from "nanoid/async";
 
 const alphabet =
-  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  "23456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 const nanoid = customAlphabet(alphabet, 15);
 
 export async function generateId(): Promise<string> {
