2dfe1750b5e11582eef66e8e514c46d5743b70d7
Robsmash138 <85640508+Robsmash138@users.noreply.github.com>
2023-05-07 00:11:10 +0000

    Clan Clash Promos

--- server/src/cardCollection/havens.ts
+++ server/src/cardCollection/havens.ts
@@ -396,7 +396,6 @@ When your Leader defeats a City Deck Mortal, you may move a Retainer from a char
     illustrator: "Mara Miranda-Escota",
     set: "Dragon & Rogue",
   },
-
   "jam-the-hermitage": {
     stack: "haven",
     name: "The Hermitage",
@@ -448,5 +447,31 @@ Reduce the [prestige] cost of Traditions you play by 1.
     `,
     illustrator: "Marco Primo",
     set: "Justice & Mercy",
+  },
+  "xxx-the-underpass": {
+    stack: "haven",
+    name: "The Underpass",
+    text: md`
+Characters in your Haven have +1 Secrecy.
+
+**Leader Ability**
+
+Pay 1 [prestige]: Mend 1 [blood] on a character in your coterie or torpor for each Animal and/or Vagrant you have attached to your Leader.
+    `,
+    illustrator: "Felipe Gaona",
+    set: "Promo",
+  },
+  "xxx-the-presidio": {
+    stack: "haven",
+    name: "The Presidio",
+    text: md`
+Characters in your Haven have +1 Secrecy.
+
+**Leader Ability**
+
+Your Leader has **+1 [shield]** vs Aggravated [damage]
+    `,
+    illustrator: "Marco Primo",
+    set: "Promo",
   },
 };
