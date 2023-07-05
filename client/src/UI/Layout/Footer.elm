e70ede95fedc1da328e11b53265d1ed63462279c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-07-04 18:17:23 -0700

    adds About link to footer.

--- client/src/UI/Layout/Footer.elm
+++ client/src/UI/Layout/Footer.elm
@@ -1,5 +1,6 @@
 module UI.Layout.Footer exposing (view)
 
+import Gen.Route as Route
 import Html exposing (Html, a, div, footer, p, small, text)
 import Html.Attributes exposing (class, href, target)
 import UI.PatreonButton
@@ -11,12 +12,16 @@ view =
         [ div [ class "footer__patreon" ]
             [ UI.PatreonButton.patreonButton
             ]
-        , small [ class "footer__legal" ]
-            [ p [] [ text "This site is not owned, endorsed or supported by Renegade Game Studios" ]
-            , p []
-                [ text "Portions of the materials are the copyrights and trademarks of Paradox Interactive AB, and are used with permission. All rights reserved. For more information please visit "
-                , a [ target "__blank", href "http://worldofdarkness.com/" ] [ text "worldofdarkness.com" ]
-                , text "."
+        , div [] 
+            [ div [ class "footer__links" ]
+                [ a [ href <| Route.toHref Route.About ] [ text "About" ] ]
+            , small [ class "footer__legal" ]
+                [ p [] [ text "This site is not owned, endorsed or supported by Renegade Game Studios" ]
+                , p []
+                    [ text "Portions of the materials are the copyrights and trademarks of Paradox Interactive AB, and are used with permission. All rights reserved. For more information please visit "
+                    , a [ target "__blank", href "http://worldofdarkness.com/" ] [ text "worldofdarkness.com" ]
+                    , text "."
+                    ]
                 ]
             ]
         ]
